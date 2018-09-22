2c197a21669e75aa2e0a902b7e1a97eb60e34403
seung-yeol <oks153123@naver.com>
2018-09-23 06:37:37 +0900

    더맵게

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
-import 코딩테스트_고득점_Kit.스택큐.다리를지나는트럭;
-import 코딩테스트_고득점_Kit.완전탐색.모의고사;
-import 코딩테스트_고득점_Kit.정렬.K번째수;
-import 코딩테스트_고득점_Kit.해시.완주하지못한선수;
+import 코딩테스트_고득점_Kit.힙.더맵게;
 
 public class Main {
 
     public static void main(String[] args) {
-        모의고사 모의고사 = new 모의고사();
-        모의고사.solution(new int[]{1,3,2,4,2});
+        더맵게 더맵게 = new 더맵게();
+        System.out.println(더맵게.solution(new int[]{1,2,3,9,10,12}, 7));
+        System.out.println(더맵게.solution(new int[]{1,5,2,5,3,9,10,12}, 7));
+        System.out.println(더맵게.solution(new int[]{6,5,4,3,3,1,2,12}, 7));
+        System.out.println(더맵게.solution(new int[]{1,5,5,6,3,5,4,1,5,9,10,12,2}, 7));
     }
 }
