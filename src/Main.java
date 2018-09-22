94dcf821aa76efea339e0c0cb83bc8cdb4dbe143
seung-yeol <oks153123@naver.com>
2018-09-22 16:43:02 +0900

    모의고사

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import 코딩테스트_고득점_Kit.스택큐.다리를지나는트럭;
+import 코딩테스트_고득점_Kit.완전탐색.모의고사;
 import 코딩테스트_고득점_Kit.정렬.K번째수;
 import 코딩테스트_고득점_Kit.해시.완주하지못한선수;
 
 public class Main {
 
     public static void main(String[] args) {
-        다리를지나는트럭 트럭 = new 다리를지나는트럭();
-        System.out.println(트럭.solution(2, 10, new int[]{7, 4, 5, 6, 10}));
+        모의고사 모의고사 = new 모의고사();
+        모의고사.solution(new int[]{1,3,2,4,2});
     }
 }
