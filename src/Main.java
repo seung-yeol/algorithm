67ddd40dadb09c37c4193e73dff469dd2f55f77d
seung-yeol <oks153123@naver.com>
2018-09-23 20:33:05 +0900

    체육복

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
+import 코딩테스트_고득점_Kit.탐욕법.체육복;
 import 코딩테스트_고득점_Kit.힙.더맵게;
 
 public class Main {
 
     public static void main(String[] args) {
-        더맵게 더맵게 = new 더맵게();
-        System.out.println(더맵게.solution(new int[]{1,2,3,9,10,12}, 7));
-        System.out.println(더맵게.solution(new int[]{1,5,2,5,3,9,10,12}, 7));
-        System.out.println(더맵게.solution(new int[]{6,5,4,3,3,1,2,12}, 7));
-        System.out.println(더맵게.solution(new int[]{1,5,5,6,3,5,4,1,5,9,10,12,2}, 7));
+        체육복 체육복 = new 체육복();
+//        체육복.solution(5, new int[]{2,4}, new int[]{1,3,5});
+        체육복.solution(5, new int[]{2,4}, new int[]{3});
+//        체육복.solution(10, new int[]{2,4,5,6,9}, new int[]{1,3,4,7,8});
     }
 }
---
+++ src/코딩테스트_고득점_Kit/탐욕법/체육복.java
@@ -0,0 +1,45 @@
+package 코딩테스트_고득점_Kit.탐욕법;
+
+import java.util.HashMap;
+
+public class 체육복 {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(i + 1, 1);
+        }
+        for (int i : lost) {
+            map.put(i, map.get(i) - 1);
+        }
+        for (int i : reserve) {
+            map.put(i, map.get(i) + 1);
+        }
+
+        int pre = map.get(1);
+        for (int i = 2; i < n + 1; i++) {
+            Integer k = map.get(i);
+
+            if (k == 0) {
+                if (pre == 2){
+                    map.put(i, 1);
+                    map.put(i-1, 1);
+                }
+                else if ( map.containsKey(i+1) && map.get(i + 1) == 2) {
+                    map.put(i, 1);
+                    map.put(i + 1, 1);
+                }
+            }
+            pre = map.get(i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (map.get(i+1) >= 1){
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
