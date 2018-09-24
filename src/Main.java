9fefec93619250d20e5603cbd4a066f761d84506
seung-yeol <oks153123@naver.com>
2018-09-25 01:25:44 +0900

    프린터

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
+import 코딩테스트_고득점_Kit.스택큐.프린터;
 import 코딩테스트_고득점_Kit.탐욕법.체육복;
 import 코딩테스트_고득점_Kit.힙.더맵게;
 
 public class Main {
 
     public static void main(String[] args) {
-        체육복 체육복 = new 체육복();
-//        체육복.solution(5, new int[]{2,4}, new int[]{1,3,5});
-        체육복.solution(5, new int[]{2,4}, new int[]{3});
-//        체육복.solution(10, new int[]{2,4,5,6,9}, new int[]{1,3,4,7,8});
+        프린터 프린터 = new 프린터();
+        System.out.println(프린터.solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 3, 5, 2, 6, 9, 3, 2, 8, 1, 9}, 9));
     }
 }
