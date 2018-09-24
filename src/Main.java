6535acc3eec3c8fb5c23aa2f741d7e49869cb224
seung-yeol <oks153123@naver.com>
2018-09-25 04:19:22 +0900

    N으로표현

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import 코딩테스트_고득점_Kit.동적프로그래밍.N으로표현;
 import 코딩테스트_고득점_Kit.스택큐.프린터;
 import 코딩테스트_고득점_Kit.탐욕법.체육복;
 import 코딩테스트_고득점_Kit.힙.더맵게;
@@ -5,7 +6,7 @@ import 코딩테스트_고득점_Kit.힙.더맵게;
 public class Main {
 
     public static void main(String[] args) {
-        프린터 프린터 = new 프린터();
-        System.out.println(프린터.solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 3, 5, 2, 6, 9, 3, 2, 8, 1, 9}, 9));
+        N으로표현 n = new N으로표현();
+        n.solution(5, 12);
     }
 }
