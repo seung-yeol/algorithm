e95ea882555acd2ad0a6528128c21e638a92259d
seung-yeol <oks153123@naver.com>
2018-09-28 22:40:27 +0900

    정수삼각형

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import 코딩테스트_고득점_Kit.동적프로그래밍.N으로표현;
+import 코딩테스트_고득점_Kit.동적프로그래밍.정수삼각형;
 import 코딩테스트_고득점_Kit.스택큐.프린터;
 import 코딩테스트_고득점_Kit.탐욕법.체육복;
 import 코딩테스트_고득점_Kit.힙.더맵게;
@@ -6,7 +7,7 @@ import 코딩테스트_고득점_Kit.힙.더맵게;
 public class Main {
 
     public static void main(String[] args) {
-        N으로표현 n = new N으로표현();
-        n.solution(5, 12);
+        정수삼각형 정 = new 정수삼각형();
+        System.out.println(정.solution(new int[][]{{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}}));
     }
 }
