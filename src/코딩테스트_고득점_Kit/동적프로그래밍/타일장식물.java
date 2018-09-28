c4b70c51a9b7477ab8fc48e099c17c60fa295e74
seung-yeol <oks153123@naver.com>
2018-09-28 20:48:40 +0900

    타일장식물

---
+++ src/코딩테스트_고득점_Kit/동적프로그래밍/타일장식물.java
@@ -0,0 +1,18 @@
+package 코딩테스트_고득점_Kit.동적프로그래밍;
+
+import java.util.ArrayList;
+
+public class 타일장식물 {
+    public long solution(int N) {
+        long preBig = 1;
+        long preSmall = 1;
+
+        for (int i = 1; i < N; i++) {
+            long newSmall = preBig;
+            preBig = preBig + preSmall;
+            preSmall = newSmall;
+        }
+
+        return preSmall * 2 + preBig * 2;
+    }
+}
